a69b14c968887f5d8714840d3bfb214f7f78f585
Võ Anh Kiệt <124515215+va-kiet@users.noreply.github.com>
2026-05-20 11:31:16 +0900

    clean and format

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -17,9 +17,9 @@ class MODE(Enum):
 
 DEFAULTS = {
     "method": "fedavg",
-    "dataset": {"name": "mnist"},
+    "dataset": {"name": "cifar10"},
     "model": {
-        "name": "lenet5",
+        "name": "avgcnn",
         "use_torchvision_pretrained_weights": True,
         "external_model_weights_path": None,
     },
@@ -99,22 +99,9 @@ DEFAULTS = {
 
 
 INPUT_CHANNELS = {
-    "mnist": 1,
-    "medmnistS": 1,
-    "medmnistC": 1,
-    "medmnistA": 1,
-    "covid19": 3,
-    "fmnist": 1,
-    "emnist": 1,
     "femnist": 1,
     "cifar10": 3,
-    "cinic10": 3,
-    "svhn": 3,
     "cifar100": 3,
-    "celeba": 3,
-    "usps": 1,
-    "tiny_imagenet": 3,
-    "domain": 3,
 }
 
 
@@ -138,43 +125,15 @@ def _get_synthetic_args():
 
 # (C, H, W)
 DATA_SHAPE = {
-    "mnist": (1, 28, 28),
-    "medmnistS": (1, 28, 28),
-    "medmnistC": (1, 28, 28),
-    "medmnistA": (1, 28, 28),
-    "fmnist": (1, 28, 28),
-    "svhn": (3, 32, 32),
-    "emnist": 62,
     "femnist": 62,
     "cifar10": (3, 32, 32),
-    "cinic10": (3, 32, 32),
     "cifar100": (3, 32, 32),
-    "covid19": (3, 244, 224),
-    "usps": (1, 16, 16),
-    "celeba": (3, 218, 178),
-    "tiny_imagenet": (3, 64, 64),
-    "synthetic": _get_synthetic_args().get("dimension", 0),
-    "domain": (3, *(_get_domainnet_args().get("image_size", (0, 0)))),
 }
 
 NUM_CLASSES = {
-    "mnist": 10,
-    "medmnistS": 11,
-    "medmnistC": 11,
-    "medmnistA": 11,
-    "fmnist": 10,
-    "svhn": 10,
-    "emnist": 62,
     "femnist": 62,
     "cifar10": 10,
-    "cinic10": 10,
     "cifar100": 100,
-    "covid19": 4,
-    "usps": 10,
-    "celeba": 2,
-    "tiny_imagenet": 200,
-    "synthetic": _get_synthetic_args().get("class_num", 0),
-    "domain": _get_domainnet_args().get("class_num", 0),
     "shakespeare": 80,
     "ucihar": 6,
     "capture24": 6
@@ -182,42 +141,16 @@ NUM_CLASSES = {
 
 
 DATA_MEAN = {
-    "mnist": [0.1307],
     "cifar10": [0.4914, 0.4822, 0.4465],
     "cifar100": [0.5071, 0.4865, 0.4409],
-    "emnist": [0.1736],
-    "fmnist": [0.286],
     "femnist": [0.9637],
-    "medmnist": [124.9587],
-    "medmnistA": [118.7546],
-    "medmnistC": [124.424],
-    "covid19": [125.0866, 125.1043, 125.1088],
-    "celeba": [128.7247, 108.0617, 97.2517],
-    "synthetic": [0.0],
-    "svhn": [0.4377, 0.4438, 0.4728],
-    "tiny_imagenet": [122.5119, 114.2915, 101.388],
-    "cinic10": [0.47889522, 0.47227842, 0.43047404],
-    "domain": [0.485, 0.456, 0.406],
 }
 
 
 DATA_STD = {
-    "mnist": [0.3015],
     "cifar10": [0.2023, 0.1994, 0.201],
     "cifar100": [0.2009, 0.1984, 0.2023],
-    "emnist": [0.3248],
-    "fmnist": [0.3205],
     "femnist": [0.155],
-    "medmnist": [57.5856],
-    "medmnistA": [62.3489],
-    "medmnistC": [58.8092],
-    "covid19": [56.6888, 56.6933, 56.6979],
-    "celeba": [67.6496, 62.2519, 61.163],
-    "synthetic": [1.0],
-    "svhn": [0.1201, 0.1231, 0.1052],
-    "tiny_imagenet": [58.7048, 57.7551, 57.6717],
-    "cinic10": [0.24205776, 0.23828046, 0.25874835],
-    "domain": [0.229, 0.224, 0.225],
 }
 
 OPTIMIZERS = {
